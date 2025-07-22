bdec0634cd6db9ae1086fc06d596a4d39bdd04b8
Diego <diego@teste.com>
2025-07-22 01:22:46 -0300

    authenticator amplify, header e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,35 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import {
+  AmplifyAuthenticatorModule,
+  AuthenticatorService,
+} from '@aws-amplify/ui-angular';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ButtonModule,
+    HeaderComponent,
+    FooterComponent,
+    AmplifyAuthenticatorModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor(public authenticator: AuthenticatorService) {
+    Amplify.configure(outputs);
+  }
   funcaoClique() {
     alert('Button clicked!');
   }
